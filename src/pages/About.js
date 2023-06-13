6d270ec6dadad8e4f8efe27eab595b5696bf17b2
DavidGobran <93219495+DavidGobran@users.noreply.github.com>
2023-06-12 22:05:04 -0400

    finished

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import "../assets/profile_pic.JPG";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              // Image goes here
+              src={require("../assets/profile_pic.JPG")}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">David Gobran</div>
+            <div className="brief_description">
+              Hi there, welcome to my page! I'm a Computer Science Junior at Worcester Polytechnic Institute.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
